f7f9fda0513ea98a8e6cfafb502b4d93a1e86d50
CHUHEI <5563338+chuhei1987@users.noreply.github.com>
2023-09-06 20:31:04 +0800

    Taking SHA1 and md5 out
    In 2004, Cryptanalyst WANG , claims her team has found that identical gemini from that md5 pool during a formal meeting in USA, since, after serval steps, md5 is said insecure.
    
    In 2005, the team has sent their analysis, so SHA1 is declared insecure.
    
    In 2017, found identical gemini from that SHA-1 pool by Google Cryptanalyst.
    
    Based on these reasoning, It is better to take these insecure beings out of the box.

--- src/cryptocalc.go
+++ src/cryptocalc.go
@@ -1,19 +1,33 @@
+/*
+
+cryptocalc.go
+
+init by  shenyp09 of github -dot- com , updated in 2016.
+
+Edited by chuhei1987 of github -dot- com aka chuhei , updated in 2023
+
+Whatever you like, caveat emptor.
+
+
+*/
+
 package main
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
+	
+	"crypto/sha256" //You may that being like sha256.h and sha256.cpp , and vice versa.
 	"crypto/sha512"
 	"fmt"
-	// "github.com/emicklei/hopwatch"
+	
 	"io"
 	"os"
 )
 
 var Usage = func() {
+	fmt.Println("cryptocalc tuned by chuhei1987 of github -dot- com aka chuhei ")
 	fmt.Println("Usage: cryptocalc command filepath/filename")
-	fmt.Println("The command are: md5 sha1 sha256 sha512")
+	//fmt.Println("The command are: md5 sha1 sha256 sha512")
+	fmt.Println("The command are: sha256 sha512")
 }
 
 func main() {
@@ -27,25 +41,19 @@ func main() {
 	infile, err := os.Open(File)
 	if err == nil {
 		switch args[1] {
-		case "md5":
-			hash := md5.New()
-			io.Copy(hash, infile)
-			fmt.Printf("MD5: %x\n", hash.Sum([]byte("")))
-		case "sha1":
-			hash := sha1.New()
-			io.Copy(hash, infile)
-			fmt.Printf("SHA1: %x\n", hash.Sum([]byte("")))
+		
+		
 		case "sha256":
 			hash := sha256.New()
 			io.Copy(hash, infile)
-			fmt.Printf("SHA256: %x\n", hash.Sum([]byte("")))
+			fmt.Printf("SHA2-256: %x\n", hash.Sum([]byte("")))
 		case "sha512":
 			hash := sha512.New()
 			io.Copy(hash, infile)
-			fmt.Printf("SHA512: %x\n", hash.Sum([]byte("")))
+			fmt.Printf("SHA2-512: %x\n", hash.Sum([]byte("")))
 		default:
 			Usage()
 		}
-	}
+	} //Implies only run once.
 	return
 }
